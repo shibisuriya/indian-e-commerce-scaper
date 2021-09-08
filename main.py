8853c8f21794b31fdab7f70db50a88f66ce1e8df
Shibi Suriya <shibisuriya.work@gmail.com>
2021-09-08 18:49:38 +0530

    Improved debugger

--- main.py
+++ main.py
@@ -20,22 +20,30 @@ while True:
 
 # Write entire page
 if(enableDebugMode == True):
-    entirePageFile = open("All HTML Pages.html", "w")
+    entirePageFile = open("debugger_output.html", "w")
     entirePageFile.write(
-        "<style>\
-            .fullPage {border: 150px solid green;} .s-desktop-width-max1 {max-width: 100%;} \
+        "<style> \
+            .fullPage {border-left: 90px solid green; border-right: 90px solid green; max-width: 100%; margin: 2em auto;} \
+            .s-desktop-width-max1 {max-width: 100%;} \
             .a-icon-alt-red {border: 5px solid red; margin: 2px; padding: 2px;} \
             .a-icon-alt-green {border: 5px solid #00FF00; margin: 2px; padding: 2px;} \
-            .pageInformation {border: 3px solid black; padding: 1em; margin: 1em;} \
-            .badge {background-color: black; border: 1px solid black; color: white; font-size:2em;border-radius:1em;} \
+            .pageInformation {border: 3px solid black; color: white; margin: 0 auto; max-width: 100%; background-color: black; padding: 1.2em; font-size: 1.5em;} \
+            .badge-red {background-color: red; border: 1px solid black; color: white; font-size:2em;border-radius:1em;} \
+            .badge-green {background-color: green; border: 1px solid black; color: white; font-size:2em;border-radius:1em;} \
+            .pageInformationChild {margin: 1em;} \
         </style>")
 
 
-def writeEntirePage(soup, productFoundInPage, uniqueProductCount, totalStarElementFoundInpage):
+def writeEntirePage(soup, productFoundInPage, uniqueProductCount, totalStarElementFoundInpage, url):
     entirePageFile.write('<div class="pageInformation">')
-    entirePageFile.write("<div>Total StarElements found in page = " + str(totalStarElementFoundInpage) + " </div>")
-    entirePageFile.write("<div>Valid products found in page = " + str(productFoundInPage) + " </div>")
+    entirePageFile.write("<div class='pageInformationChild'>Total StarElements found in page = " +
+                         str(totalStarElementFoundInpage) + " </div>")
+    entirePageFile.write(
+        "<div class='pageInformationChild'>Valid products found in page = " + str(productFoundInPage) + " </div>")
+    entirePageFile.write("<div class='pageInformationChild'>URL = " + str(url) + " </div>")
     entirePageFile.write("</div>")
+
+    # Remove unwanted elements.
     soup.find("header", {"id": "navbar-main"}).decompose()
     soup.find("div", {"id": "navFooter"}).decompose()
     soup.find(class_="a-dropdown-container").decompose()
@@ -45,16 +53,15 @@ def writeEntirePage(soup, productFoundInPage, uniqueProductCount, totalStarEleme
         class_="s-desktop-width-max s-desktop-content s-opposite-dir sg-row")['class'] = "s-desktop-width-max1 s-desktop-content s-opposite-dir sg-row"
     icons = soup.find_all(class_='a-icon-alt')
     for index, icon in enumerate(icons):
-        badge = soup.new_tag('span', attrs={"class": "badge"})
-        badge.string = str(index + 1)
-
         if('&' not in str(icon)):
+            badge = soup.new_tag('span', attrs={"class": "badge-green"})
+            badge.string = str(index + 1)
             icon.parent.parent['class'] = 'a-icon-alt-green'
             icon.parent.append(badge)
-            print(icon.parent.parent.parent)
 
-            # print(test)
         else:
+            badge = soup.new_tag('span', attrs={"class": "badge-red"})
+            badge.string = str(index + 1)
             icon.parent.parent['class'] = 'a-icon-alt-red'
             icon.parent.parent.parent.append(badge)
 
@@ -227,7 +234,8 @@ for key in keyList:
                         productJson, ignore_index=True)
                     uniqueProductCount += 1
         if(enableDebugMode == True):
-            writeEntirePage(soup, productFoundInPage, uniqueProductCount, len(products))
+            writeEntirePage(soup, productFoundInPage,
+                            uniqueProductCount, len(products), url)
         print("Found ", productFoundInPage, " products in ", url,
               " out of which ", uniqueProductCount, " are new.")
 print(productCollection)
