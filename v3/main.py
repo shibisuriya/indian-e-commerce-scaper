5e773ea02f6bf759f067d6bdab77788f212c1c7e
Shibi Suriya <shibisuriya.work@gmail.com>
2023-05-16 14:22:21 +0530

    fix: stable

--- v3/main.py
+++ v3/main.py
@@ -1,33 +1,70 @@
 import csv
 import re
-from utils import get_star_elements
+from utils import get_star_elements, get_soup, is_last_page, get_key 
+import os
+
+
+
+
+
+# Reset all formatting
+RESET = "\033[0m"
+
+# Colors
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+
+# Example usage
+
 hash = {}
-with open('./data.csv', "w") as file:
+page_number = 1
+file_name = './data.csv' 
+if os.path.isfile(file_name):
+    # File exists, proceed with reading and writing
+    with open(file_name, "r") as file:
+        reader = csv.reader(file, delimiter='|')
+        existing_items = list(reader)
+        for item in existing_items:
+            title, image, *_ = item 
+            if(get_key(title, image) not in hash):
+                hash[get_key(title, image)] = item
+            else:
+                print(RED + 'The file is corrupt! It contains duplicate elements! Delete the file to continue!', RESET)
+
+with open(file_name, "a") as file:
     writer = csv.writer(file, delimiter='|')
-    writer.writerow(['title', 'image', 'rating', 'number of reviews'])
-    elements = get_star_elements() 
-    for index, element in enumerate(elements):
-        item = element.parent.parent.parent.parent.parent.parent.parent.parent.parent
+    while True:
+        page = get_soup(page_number)
+        elements = get_star_elements(page) 
+        print(YELLOW + f"Found {len(elements)} products in page {page_number}." + RESET)
+        for index, element in enumerate(elements):
+            item = element.parent.parent.parent.parent.parent.parent.parent.parent.parent
+            image = item.find('img', class_='s-image') 
+            if(image):
+                image = image['src']
 
-        image = item.find('img', class_='s-image') 
-        if(image):
-            image = image['src']
+            title = item.find('span', class_='a-size-base-plus a-color-base a-text-normal') 
+            if(title):
+                title = title.text
+            else: 
+                title = item.find('span', class_="a-size-medium a-color-base a-text-normal") 
+                title = title.text
+            
+            rating = item.find('span', class_='a-icon-alt').text
+            rating = re.search(r'(.+?) out', rating).group(1).strip()
 
-        title = item.find('span', class_='a-size-base-plus a-color-base a-text-normal') 
-        if(title):
-            title = title.text
-        else: 
-            title = item.find('span', class_="a-size-medium a-color-base a-text-normal") 
-            title = title.text
-        
-        rating = item.find('span', class_='a-icon-alt').text
-        rating = re.search(r'(.+?) out', rating).group(1).strip()
+            number_of_reviews = item.find('span', class_="a-size-base s-underline-text").text
 
-        number_of_reviews = item.find('span', class_="a-size-base s-underline-text").text
+            if(get_key(title, image) not in hash):
+                print(BLUE + f"Found new item, {title}." + RESET)
+                hash[get_key(title, image)] = [ title, image, rating, number_of_reviews]
+                writer.writerow([ title, image, rating, number_of_reviews])
 
-        if(f"{title}-{image}" not in hash):
-            hash[f"{title}-{image}"] = [ title, image, rating, number_of_reviews]
-            writer.writerow([ title, image, rating, number_of_reviews])
-        else:
-            print(f"Item {title} already exists in the hash!")
-
+        if(is_last_page(page)):
+            break
+        page_number += 1
--- v3/utils.py
+++ v3/utils.py
@@ -1,6 +1,18 @@
 from key import key
 import requests
+import random  
 from bs4 import BeautifulSoup
+
+
+def get_key(title, image):
+    return f"{title}-{image}"
+
+def specific_string(length):  
+    sample_string = 'pqrstuvwxyaksdjhkasdlkjqluwoelkansldknc' # define the specific string  
+    # define the condition for random string  
+    result = ''.join((random.choice(sample_string)) for x in range(length))  
+    return result
+
 cookies = {
     'session-id': '261-1163646-2292460',
     'session-id-time': '2082787201l',
@@ -11,32 +23,8 @@ cookies = {
 }
 
 headers = {
-    'authority': 'www.amazon.in',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-GB,en;q=0.9',
-    'cache-control': 'no-cache',
-    # 'cookie': 'session-id=261-1163646-2292460; session-id-time=2082787201l; i18n-prefs=INR; ubid-acbin=262-5280853-8370053; session-token=0Yx/sl7jsWUnMhSpZuNhnhoitPCy/P87wflbutp1yQrh8Yb7bcu/iy3Pq6+mwfvbHPt5levtC+EpVnMKlrXlieqJfd4SHBcO9/fKvQVmGwljmQpqwB64900L33gAXKg3/CY2GA19uLkuW5syRtu0GcyU6SvUfupuV4+E0Rd4U5kuWuzVWlNYH/uGVOJA2vhioSLTEh56fG2jrmtzmwZXX9qsZVVQjGaaqT729Q99uP8=; csm-hit=tb:s-RCA08544A2YQCT2J63XM|1684211284757&t:1684211285532&adb:adblk_no',
-    'device-memory': '8',
-    'downlink': '10',
-    'dpr': '2',
-    'ect': '4g',
-    'pragma': 'no-cache',
-    'rtt': '150',
-    'sec-ch-device-memory': '8',
-    'sec-ch-dpr': '2',
-    'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-ch-ua-platform-version': '"12.4.0"',
-    'sec-ch-viewport-width': '1440',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'service-worker-navigation-preload': 'true',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-    'viewport-width': '1440',
+            'User-Agent': specific_string(random.randint(1,999)),
+            'From': specific_string(random.randint(1,999)) 
 }
 
 params = {
@@ -45,13 +33,19 @@ params = {
 
 
 def is_last_page(page):
-    # A page is last if the 'next' button in the paginator is disabled! 
-    return bool(page.find('span', class_='s-pagination-disabled'))
+    next_button = page.find('a', class_="s-pagination-next")
+    if(next_button):
+        classes_list = next_button['class']
+        return bool('s-pagination-disabled' in classes_list)
+    else:
+        return True
+    
 
-def get_soup(): 
+
+def get_soup(page_number): 
+    params['page'] = str(page_number)
     response = requests.get('https://www.amazon.in/s', params=params, cookies=cookies, headers=headers)
     return BeautifulSoup(response.text, 'html.parser')
 
-def get_star_elements():
-    soup = get_soup()
+def get_star_elements(soup):
     return soup.find_all(class_='a-icon-star-small')
